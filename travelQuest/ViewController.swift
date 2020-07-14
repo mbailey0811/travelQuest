3a09889586b8adef236d917d64e0a1bf3df1cb16
Megan Bailey <mbailey0811@yahoo.com>
2020-07-14 11:07:44 -0400

    added button

--- travelQuest/ViewController.swift
+++ travelQuest/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var userGreetingHomePage: UILabel!
+    
+    @IBOutlet weak var chooseApocalypseGame: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        userGreetingHomePage.isHidden = true
+        chooseApocalypseGame.isHidden = true
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedSubmitUserName(_ sender: UIButton) {
+        if let userName = userNameTextField.text {
+        userGreetingHomePage.text = "\(userName), please pick a story from below."
+        }
+        userGreetingHomePage.isHidden = false
+        chooseApocalypseGame.isHidden = false
+    }
+    
 }
 
