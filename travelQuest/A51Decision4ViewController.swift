52c7c4446110ba5888d69fa230d052f7fddaa357
Megan Bailey <mbailey0811@yahoo.com>
2020-07-15 20:31:01 -0400

    all view controllers now set up

---
+++ travelQuest/A51Decision4ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  A51Decision4ViewController.swift
+//  travelQuest
+//
+//  Created by Christine Bailey on 7/15/20.
+//  Copyright © 2020 Megan Bailey. All rights reserved.
+//
+
+import UIKit
+
+class A51Decision4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
